42bd4c24c79424253c14e5a2095863692747d08f
Илья С <skachilya@gmail.com>
2024-12-06 16:08:50 +0200

    test 5

--- src/index.js
+++ src/index.js
@@ -19,6 +19,23 @@ if (tg !== undefined) {
   }
 }
 
+(function() {
+  var { retrieveLaunchParams, postEvent } = window.telegramApps.sdk;
+  var lp = retrieveLaunchParams();
+
+  // Некоторые версии Telegram не требуют классов выше.
+  if (['macos', 'tdesktop', 'weba', 'web', 'webk'].includes(lp.platform)) {
+    return;
+  }
+
+  // Расширить приложение.
+  postEvent('web_app_expand');
+
+  document.body.classList.add('mobile-body');
+  document.getElementById('wrap').classList.add('mobile-wrap');
+  document.getElementById('content').classList.add('mobile-content');
+})();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
